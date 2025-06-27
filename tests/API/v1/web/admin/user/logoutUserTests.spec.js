89ee7d71ed5f988b316ba4e566b5130af4be19ef
kkamara <kamaracomputers@gmail.com>
2025-06-27 18:01:53 +0100

    Add web API admin tests

---
+++ tests/API/v1/web/admin/user/logoutUserTests.spec.js
@@ -0,0 +1,76 @@
+'use strict';
+const assert = require('node:assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const config = require('../../../../../src/config');
+const db = require("../../../../../src/models/v1");
+
+chai.use(chaiHttp);
+
+const app = `http://localhost:${config.appPort}/api/v1/web/admin`;
+
+let createdAccountID = null;
+let bearerToken = null;
+let authTokenID = null;
+
+const payload = {
+  email: "testaccount@example.com",
+  firstName: "Test",
+  lastName: "Account",
+  dob: "2004-01-01",
+  password: "secret",
+  passwordConfirmation: "secret",
+  role: "guest",
+};
+
+describe('Logout User Web API Tests', function() {
+  before(async function() {
+    const createdAccount = await db.sequelize.models
+      .user
+      .testCreateUser(payload, true);
+    createdAccountID = createdAccount.userId;
+    
+    const createdUserToken = await db.sequelize.models
+      .userToken
+      .createAuthToken(createdAccountID);
+    authTokenID = createdUserToken.authTokenId;
+    
+    const userToken = await db.sequelize.models
+      .userToken
+      .getAuthToken(authTokenID);
+    bearerToken = "Bearer "+userToken.token;
+  });
+  it('Tests Logout User Success', function(done) {
+    chai.request(app)
+      .delete('/user')
+      .set("authorization", bearerToken)
+      .end(async (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+        chai.expect(err).to.be.null;
+        chai.expect(res).to.have.status(200);
+        chai.expect(res.body).to.have.property('message');
+        chai.expect(res.body.message).to.equal("Success");
+
+        const authenticated = await db.sequelize.models
+          .userToken
+          .authenticate(
+            bearerToken,
+          );
+        chai.expect(authenticated).to.equal(false);
+
+        done();
+      });
+  });
+  after(async function() {
+    await db.sequelize.models
+      .userToken
+      .testDeleteUserToken(
+        authTokenID,
+      );
+    await db.sequelize.models
+      .user
+      .testDeleteUser(createdAccountID);
+  });
+});
