09fc54841c13cd0cec4953cee4d00af6bc60bc0c
kkamara <kamaracomputers@gmail.com>
2025-06-30 18:28:44 +0100

    Prepare dashboard statistics

--- frontend/src/components/pages/adminDashboard/HomeComponent.jsx
+++ frontend/src/components/pages/adminDashboard/HomeComponent.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
-import { Helmet } from "react-helmet"
-import { authorize } from "../../../redux/actions/adminAuthActions"
-import Error from "../../layouts/Error"
-import { adminDashboardTitle, } from "../../../constants"
+import React, { useEffect, } from "react";
+import { useNavigate, } from "react-router-dom";
+import { useDispatch, useSelector, } from "react-redux";
+import { Helmet } from "react-helmet";
+import { authorize, } from "../../../redux/actions/adminAuthActions";
+import Error from "../../layouts/Error";
+import { adminDashboardTitle, } from "../../../constants";
 
 export default function HomeComponent() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const state = useSelector((state) => ({
     adminAuth: state.adminAuth,
-  }))
+  }));
 
   useEffect(() => {
-    dispatch(authorize())
-  }, [])
+    dispatch(authorize());
+  }, []);
 
   useEffect(() => {
     if (false === state.adminAuth.loading && !state.adminAuth.data) {
-      window.location.href = "/admin/signin"
+      window.location.href = "/admin/signin";
     }
-  }, [state.adminAuth])
+  }, [state.adminAuth]);
 
   if (state.adminAuth.loading) {
     return (
@@ -32,7 +32,7 @@ export default function HomeComponent() {
         </Helmet>
         <p>Loading...</p>
       </div>
-    )
+    );
   }
 
   return (
@@ -49,10 +49,102 @@ export default function HomeComponent() {
       </div>
 
       <div className="row">
-        <div className="col-md-12">
-          <pre>Charts and statistics go here.</pre>
+        <div className="col-xl-3 col-md-6 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                    Products
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    PRODUCTS_COUNT
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-md-6 mb-4">
+          <div className="card border-left-success shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                    Orders (Month)
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    MONTH_ORDERS_COUNT
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-md-6 mb-4">
+          <div className="card border-left-info shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
+                    Ongoing Shipments
+                  </div>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                        50%
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="progress progress-sm mr-2">
+                        <div
+                          className="progress-bar bg-info"
+                          role="progressbar"
+                          style={{ width: "50%" }}
+                          aria-valuenow="50"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-md-6 mb-4">
+          <div className="card border-left-warning shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                    Users
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    USERS_COUNT
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-comments fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
