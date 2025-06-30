73cc60ce2ce411ce973aa9ac41fdf4953fae3d87
kkamara <kamaracomputers@gmail.com>
2025-06-30 23:12:52 +0100

    Add adminAuthenticate() middleware

--- src/routes/api/v1/web/stats/admin/products/products.js
+++ src/routes/api/v1/web/stats/admin/products/products.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { status, } = require("http-status");
-const authenticate = require("../../../../../../../middlewares/v1/authenticate");
+const adminAuthenticate = require("../../../../../../../middlewares/v1/adminAuthenticate");
 const db = require("../../../../../../../models/v1/index");
 const { message500 } = require("../../../../../../../utils/httpResponses");
 
 const router = express.Router();
 
-router.get("/", authenticate, async (req, res) => {
+router.get("/", adminAuthenticate, async (req, res) => {
   const result = await db.sequelize.models.product.getAdminProductCountStat();
   if (false === result) {
     res.status(status.UNAUTHORIZED);
