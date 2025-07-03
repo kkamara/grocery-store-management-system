8921adf57f827def47b57e6cbe1c26b0337ce7c9
kkamara <kamaracomputers@gmail.com>
2025-07-03 20:50:28 +0100

    Rename guest users stat title on dashboard screen

--- frontend/src/components/pages/adminDashboard/HomeComponent.jsx
+++ frontend/src/components/pages/adminDashboard/HomeComponent.jsx
@@ -140,7 +140,7 @@ export default function HomeComponent() {
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                    Users
+                    Guest Users
                   </div>
                   <div className="h5 mb-0 font-weight-bold text-gray-800">
                     USERS_COUNT
