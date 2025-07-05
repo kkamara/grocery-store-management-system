dbf982aa758b8d08fd369c9057cc2bd17398ba92
kkamara <kamaracomputers@gmail.com>
2025-07-05 19:27:05 +0100

    Add demo-product seeders

---
+++ src/seeders/v1/20250705172716-demo-categories.js
@@ -0,0 +1,40 @@
+'use strict';
+const moment = require("moment-timezone");
+const { faker, } = require('@faker-js/faker');
+const { mysqlTimeFormat, } = require("../../utils/time");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const insertArray = [];
+      for (let i = 0; i < 15; i++) {
+        insertArray.push({
+          name: faker.internet.displayName(),
+          createdAt: moment().utc().format(mysqlTimeFormat),
+          updatedAt: moment().utc().format(mysqlTimeFormat),
+        });
+      }
+      await queryInterface.bulkInsert(
+        'categories',
+        insertArray,
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  down: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.bulkDelete('categories', null, { transaction, });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
---
+++ src/seeders/v1/20250705173423-demo-products.js
@@ -0,0 +1,88 @@
+'use strict';
+const moment = require("moment-timezone");
+const { faker, } = require('@faker-js/faker');
+const { mysqlTimeFormat, } = require("../../utils/time");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const insertArray = [];
+      for (let i = 0; i < 30; i++) {
+        const category = await queryInterface.sequelize.query(
+          "SELECT id FROM categories ORDER BY rand() LIMIT 1",
+          { type: Sequelize.QueryTypes.SELECT },
+        );
+
+        if (0 === category.length) {
+          throw new Error("No categories are available to select.");
+        }
+
+        const manufacturer = await queryInterface.sequelize.query(
+          "SELECT id FROM manufacturers ORDER BY rand() LIMIT 1",
+          { type: Sequelize.QueryTypes.SELECT },
+        );
+
+        if (0 === manufacturer.length) {
+          throw new Error("No manufacturers are available to select.");
+        }
+
+        const productInsertResult = await queryInterface.sequelize.query(
+          `INSERT INTO products(name, units, weight, categoriesId, price, description, manufacturersId, createdAt, updatedAt)
+            VALUES (:name, :units, :weight, :categoriesId, :price, :description, :manufacturersId, :createdAt, :updatedAt)`,
+          {
+            replacements: {
+              name: faker.commerce.productName(),
+              units: faker.number.int({ min: 0, max: 1000, }),
+              weight: faker.number.int({ min: 0, max: 100, }) + " kg",
+              categoriesId: category[0].id,
+              price: faker.number.float({ min: 0.1, max: 200, }),
+              description: faker.number.int({ min: 0, max: 1, }) == 1 ?
+                faker.lorem.paragraphs({ min: 1, max: 1, }, "\n") :
+                null,
+              manufacturersId: manufacturer[0].id,
+              createdAt: moment().utc().format(mysqlTimeFormat),
+              updatedAt: moment().utc().format(mysqlTimeFormat),
+            },
+            type: Sequelize.QueryTypes.INSERT,
+          }
+        );
+        const productsId = productInsertResult[0];
+        for (let k = 0; k < faker.number.int({ min: 1, max: 5, }); k++) {
+          const photo = "image string";
+          await queryInterface.sequelize.query(
+            `INSERT INTO productPhotos(productsId, name, photo, type, createdAt, updatedAt)
+              VALUES (:productsId, :name, :photo, :type, :createdAt, :updatedAt)`,
+            {
+              replacements: {
+                name: faker.commerce.productName(),
+                photo: photo,
+                type: "image/png",
+                createdAt: moment().utc().format(mysqlTimeFormat),
+                updatedAt: moment().utc().format(mysqlTimeFormat),
+                productsId,
+              },
+              type: Sequelize.QueryTypes.INSERT,
+            }
+          );
+        }
+      }
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  down: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.bulkDelete('productPhotos', null, { transaction, });
+      await queryInterface.bulkDelete('products', null, { transaction, });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
