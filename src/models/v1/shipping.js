0afb26d11ee175c27d6f2f21067b91611673dec4
kkamara <kamaracomputers@gmail.com>
2025-07-06 18:28:46 +0100

    Improve number rounding

--- src/models/v1/shipping.js
+++ src/models/v1/shipping.js
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
         
         let result = (part[0].count / whole[0].count) * 100;
         if (0 < result) {
-          result = Math.round(result * 100) / 100;
+          result = Math.round((result + Number.EPSILON) * 100) / 100;
         }
         return { percentage: result + "%" };
       } catch(err) {
