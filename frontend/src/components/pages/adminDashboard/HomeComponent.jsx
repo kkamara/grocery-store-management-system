ac7a0311c788943fbb5802b790d95d4b2f29da85
kkamara <kamaracomputers@gmail.com>
2025-07-06 21:59:31 +0100

    Improve design of dashboard statistics

--- frontend/src/components/pages/adminDashboard/HomeComponent.jsx
+++ frontend/src/components/pages/adminDashboard/HomeComponent.jsx
@@ -25,6 +25,8 @@ import { getAdminGuestUsersCountStat, } from "../../../redux/actions/adminGuestU
 import { getAdminEarningsLineChart, } from "../../../redux/actions/adminEarningsLineChartActions"
 import { getAdminOrdersBarChart, } from "../../../redux/actions/adminOrdersBarChartActions"
 
+import "./HomeComponent.scss"
+
 ChartJS.register(
   ArcElement,
   Tooltip,
@@ -102,7 +104,7 @@ export default function HomeComponent() {
       <div className="row">
         <div className="col-xl-3 col-md-6 mb-4">
           <div className="card border-left-primary shadow h-100 py-2">
-            <div className="card-body">
+            <div className="card-body dashboard-statistic-card-body">
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
@@ -122,7 +124,7 @@ export default function HomeComponent() {
 
         <div className="col-xl-3 col-md-6 mb-4">
           <div className="card border-left-success shadow h-100 py-2">
-            <div className="card-body">
+            <div className="card-body dashboard-statistic-card-body">
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
@@ -142,7 +144,7 @@ export default function HomeComponent() {
 
         <div className="col-xl-3 col-md-6 mb-4">
           <div className="card border-left-info shadow h-100 py-2">
-            <div className="card-body">
+            <div className="card-body dashboard-statistic-card-body">
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
@@ -178,7 +180,7 @@ export default function HomeComponent() {
 
         <div className="col-xl-3 col-md-6 mb-4">
           <div className="card border-left-warning shadow h-100 py-2">
-            <div className="card-body">
+            <div className="card-body dashboard-statistic-card-body">
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
