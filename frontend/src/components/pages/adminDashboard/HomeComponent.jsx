dd4d2de57c1d99d2f9e2c516ec152f9bca0a1eaf
kkamara <kamaracomputers@gmail.com>
2025-07-12 20:05:21 +0100

    Set class to main element in dashboard HomeComponent

--- frontend/src/components/pages/adminDashboard/HomeComponent.jsx
+++ frontend/src/components/pages/adminDashboard/HomeComponent.jsx
@@ -89,7 +89,7 @@ export default function HomeComponent() {
   }
   
   return (
-    <div className="container-fluid">
+    <div className="container-fluid dashboard-home-container">
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
         <a
