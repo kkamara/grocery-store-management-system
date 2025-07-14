4fe130808277577832f06f052745aacc1033b621
kkamara <kamaracomputers@gmail.com>
2025-07-14 17:04:17 +0100

    Prepare admin view products screen

--- frontend/src/components/pages/adminDashboard/products/ViewProductsComponent.jsx
+++ frontend/src/components/pages/adminDashboard/products/ViewProductsComponent.jsx
@@ -13,7 +13,18 @@ export default function ViewProductsComponent() {
   return (
     <div className="container-fluid">
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">ViewProductsComponent</h1>
+        <h1 className="h3 mb-0 text-gray-800">View Products</h1>
+      </div>
+      
+      <div className="row">
+
+        <div className="col-xl-12 col-lg-12">
+          <div className="card shadow mb-4">
+            <div className="card-body">
+              <p>Show Products Table</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
