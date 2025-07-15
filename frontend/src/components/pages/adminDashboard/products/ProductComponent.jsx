98d98dfad324222d3ee64f80013f12fa441fbf89
kkamara <kamaracomputers@gmail.com>
2025-07-15 17:29:57 +0100

    Automatically change slides

--- frontend/src/components/pages/adminDashboard/products/ProductComponent.jsx
+++ frontend/src/components/pages/adminDashboard/products/ProductComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet, } from "react-helmet"
-import { Slide, } from "react-slideshow-image"
+import { Fade, } from "react-slideshow-image"
 import { adminDashboardTitle, } from "../../../../constants"
 
 import "react-slideshow-image/dist/styles.css"
@@ -11,23 +11,23 @@ const indicators = (index) => (<div className="indicator">{index + 1}</div>);
 export default function ProductComponent() {
 
   const renderProductPhotos = () => {
-    return <Slide indicators={indicators}>
+    return <Fade indicators={indicators}>
         <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(http://localhost:8000/productPhotos/pizza-6948995_1280.webp)` }}>
-                <span>Slide 1</span>
+                <span>Photo 1</span>
             </div>
         </div>
         <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(http://localhost:8000/productPhotos/apples-2243734_1280.jpg)` }}>
-                <span>Slide 2</span>
+                <span>Photo 2</span>
             </div>
         </div>
         <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(http://localhost:8000/productPhotos/dairy-5621769_1280.webp)` }}>
-                <span>Slide 3</span>
+                <span>Photo 3</span>
             </div>
         </div>
-    </Slide>
+    </Fade>
   }
 
   return (
