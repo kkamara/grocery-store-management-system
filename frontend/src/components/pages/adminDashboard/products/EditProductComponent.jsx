5d155a0c7caacd3377f0fb72a067444cfec9959e
kkamara <kamaracomputers@gmail.com>
2025-08-27 14:44:32 +0100

    Update title for admin edit product screen 2

--- frontend/src/components/pages/adminDashboard/products/EditProductComponent.jsx
+++ frontend/src/components/pages/adminDashboard/products/EditProductComponent.jsx
@@ -309,7 +309,7 @@ export default function EditProductComponent() {
         <title>
           Edit {state.adminGetProduct.data.name} - {adminDashboardTitle}
         </title>
-        <meta name="title" content={`Edit Product - ${adminDashboardTitle}`}/>
+        <meta name="title" content={`Edit ${state.adminGetProduct.data.name} - ${adminDashboardTitle}`}/>
       </Helmet>
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 className="h3 mb-0 text-gray-800">
