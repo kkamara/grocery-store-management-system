3940cbe4a14a642cf8a1b61f329a0af5dfe6de3e
kkamara <kamaracomputers@gmail.com>
2025-09-02 16:49:42 +0100

    Load cart data on cart screen

--- frontend/src/components/pages/cart/CartComponent.jsx
+++ frontend/src/components/pages/cart/CartComponent.jsx
@@ -1,8 +1,36 @@
-import React from "react"
+import React, { useEffect, } from "react"
+import { useSelector, useDispatch, } from "react-redux"
+import { Helmet, } from "react-helmet"
+import { getCart, } from "../../../redux/actions/cartActions"
 
 export default function CartComponent() {
+  const dispatch = useDispatch()
+  const state = useSelector(state => ({
+    cart: state.cart,
+  }))
+
+  useEffect(() => {
+    dispatch(getCart())
+  }, [])
+
+  console.log(state.cart)
+
+  if (state.cart.loading) {
+    return (
+      <div className="container cart-container text-center">
+        <Helmet>
+          <title>Your Cart - {process.env.REACT_APP_NAME}</title>
+        </Helmet>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="container cart-container">
+      <Helmet>
+        <title>Your Cart - {process.env.REACT_APP_NAME}</title>
+      </Helmet>
       CartComponent
     </div>
   )
