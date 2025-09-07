1d20252915b37e15a9d65b8458f4b226fb85fef8
kkamara <kamaracomputers@gmail.com>
2025-09-07 15:07:56 +0100

    Hook up checkout request to checkout success screen

--- frontend/src/components/pages/checkout/CheckoutSuccessComponent.jsx
+++ frontend/src/components/pages/checkout/CheckoutSuccessComponent.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { useParams, } from 'react-router'
+import React, { useEffect, } from 'react'
+import { useNavigate, useParams, } from 'react-router'
+import { useDispatch, useSelector, } from "react-redux"
+import { getCheckout } from '../../../redux/actions/checkoutActions'
 
 export default function CheckoutSuccessComponent() {
   const { billingReference } = useParams()
+  const dispatch = useDispatch()
+  const state = useSelector(state => ({
+    checkout: state.checkout,
+  }))
+  const navigate = useNavigate()
 
-  console.log(billingReference)
+  useEffect(() => {
+    dispatch(getCheckout(billingReference))
+  }, [])
+
+  useEffect(() => {
+    if (false === state.checkout.loading) {
+      if (null !== state.checkout.error) {
+        return navigate("/404-not-found")
+      }
+    }
+  }, [state.checkout])
+
+  console.log(state.checkout)
   
   return (
     <div className="container checkout-success-container">
