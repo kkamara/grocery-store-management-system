245488993a32ab9de496332eb4055c0b1b39e165
kkamara <kamaracomputers@gmail.com>
2025-09-07 15:19:30 +0100

    Design checkout success screen

--- frontend/src/components/pages/checkout/CheckoutSuccessComponent.jsx
+++ frontend/src/components/pages/checkout/CheckoutSuccessComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, } from 'react'
 import { useNavigate, useParams, } from 'react-router'
 import { useDispatch, useSelector, } from "react-redux"
+import { Helmet, } from "react-helmet"
 import { getCheckout } from '../../../redux/actions/checkoutActions'
 
 export default function CheckoutSuccessComponent() {
@@ -23,11 +24,37 @@ export default function CheckoutSuccessComponent() {
     }
   }, [state.checkout])
 
-  console.log(state.checkout)
+  if (state.checkout.loading) {
+    return (
+      <div className="container product-container text-center">
+        <Helmet>
+          <title>Checkout - {process.env.REACT_APP_NAME}</title>
+        </Helmet>
+        <p>Loading...</p>
+      </div>
+    )
+  }
   
   return (
     <div className="container checkout-success-container">
-      <div>CheckoutSuccessComponent</div>
+      <Helmet>
+        <title>Checkout Order {state.checkout.data.billingReference} - {process.env.REACT_APP_NAME}</title>
+      </Helmet>
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <h1>Order {state.checkout.data.billingReference}</h1>
+
+          <p>
+            <strong>Payment Method:</strong> {state.checkout.data.paymentMethod}
+          </p>
+          <p>
+            <strong>Total Price:</strong> {state.checkout.data.amount}
+          </p>
+          <p>
+            <strong>Shipping Status:</strong> {state.checkout.data.shipping && state.checkout.data.shipping.status}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
